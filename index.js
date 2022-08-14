e80cf3326845a19dfa4b0ee70220c4a7877f104b
Mirsen Kahrovic <mirsenkahrovic@gmail.com>
2022-08-14 12:01:24 +0200

    adding file uploads

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const fileUpload = require('express-fileupload')
+
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const path = require('path')
 const db = require('./db.js')
+
 const app = express();
+
 const PORT = 8080;
 
 // Middleware
@@ -18,9 +24,34 @@ app.use(
   })
 );
 
+app.use(
+  fileUpload({
+    limits: {
+      fileSize: 1024 * 1024 * 100
+    },
+    abortOnLimit: true,
+    createParentPath: true,
+  })
+);
+
 // Routes
 app.get('/boards', db.getBoards)
 app.get('/boards/:id', db.getBoardById)
+app.post("/file", (req, res) => {
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  const file = req.files.myFile;
+  const path = __dirname + "/file/" + file.name;
+
+  file.mv(path, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.send({ status: "success", path: path });
+  });
+});
 app.post('/boards', db.createBoard)
 app.put('/boards/:id', db.updateBoard)
 app.delete('/boards/:id', db.deleteBoard)
